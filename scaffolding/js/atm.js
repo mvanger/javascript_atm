e57e8d362f8c941329f073b2514a6a69eb7dd3c6
mvanger <mvanger@nyu.edu>
2013-07-23 17:14:28 -0400

    Can deposit 32965$ into checking

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -1,15 +1,16 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
 // The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM, 
+// At this point, all of the objects in the document are in the DOM,
 // and all the images and sub-frames have finished loading.
 
 window.onload = function(){
 
   // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-  // The click event is raised when the user clicks on an element. 
+  // The click event is raised when the user clicks on an element.
 
   document.getElementById("checkingDeposit").onclick = function(event){
     // Any code you put in here will be run when the checkingDeposit button is clicked
+    document.getElementById("checkingBalance").textContent = "$" + (parseInt(document.getElementById("checkingBalance").textContent.slice(1),10) + parseInt(document.getElementById("checkingAmount").value));
   };
 
   document.getElementById("savingsDeposit").onclick = function(event){
