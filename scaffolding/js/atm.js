60d8c880364e5e1615cd80d188ff04917faed87a
Clearfield Christopher <chris.clearfield@gmail.com>
2013-07-23 15:29:04 -0400

    initial checkin

---
+++ scaffolding/js/atm.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+  document.getElementById("checkingDeposit").onclick = function(event){
+  // Any code you put in here will be run when the checkingDeposit button is clicked
+  };
+
+  document.getElementById("savingsDeposit").onclick = function(event){
+  // Any code you put in here will be run when the savingsDeposit button is clicked
+  };
+
+  document.getElementById("checkingWithdraw").onclick = function(event){
+  // Any code you put in here will be run when the checkingWithdraw button is clicked
+  };
+
+  document.getElementById("savingsWithdraw").onclick = function(event){
+  // Any code you put in here will be run when the savingsWithdraw button is clicked
+  };
+
+};
+
