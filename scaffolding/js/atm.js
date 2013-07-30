7aa4483f15ad67b81b54e161e6df923a14f63ca3
mvanger <mvanger@nyu.edu>
2013-07-30 15:33:15 -0400

    Did more jQuery refactoring

--- scaffolding/js/atm.js
+++ scaffolding/js/atm.js
@@ -1,58 +1,55 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
-// The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM,
-// and all the images and sub-frames have finished loading.
-
-window.onload = function(){
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-  // The click event is raised when the user clicks on an element.
+$(function(){
+  $("#checkingDeposit").click(depositChecking)
+  $("#savingsDeposit").click(depositSavings)
+  $("#checkingWithdraw").click(withdrawChecking)
+  $("#savingsWithdraw").click(withdrawSavings)
 
   var checkingBalance = $("#checkingBalance")[0];
-  var checkingAmount = $("#checkingAmount");
   var savingsBalance = $("#savingsBalance")[0];
-  var savingsAmount = $("#savingsAmount");
-
   checkingBalance.style.background = "red";
   savingsBalance.style.background = "red";
 
-  $("#checkingDeposit").click(function(event){
-    // Any code you put in here will be run when the checkingDeposit button is clicked
-    checkingBalance.textContent = "$" + (parseInt(checkingBalance.textContent.slice(1,10)) + parseInt(checkingAmount.val()));
-    if (checkingBalance.textContent !== "$0") {
-      checkingBalance.style.background = "#E3E3E3";
-    }
-  });
+})
 
-  $("#savingsDeposit").click(function(event){
-    // Any code you put in here will be run when the savingsDeposit button is clicked
-    savingsBalance.textContent = "$" + (parseInt(savingsBalance.textContent.slice(1),10) + parseInt(savingsAmount.val()));
-    if (savingsBalance.textContent !== "$0") {
-      savingsBalance.style.background = "#E3E3E3";
-    }
-  });
+function depositChecking(){
+  var checkingBalance = $("#checkingBalance")[0];
+  var checkingAmount = $("#checkingAmount");
+  checkingBalance.textContent = "$" + (parseInt(checkingBalance.textContent.slice(1,10)) + parseInt(checkingAmount.val()));
+  if (checkingBalance.textContent !== "$0") {
+    checkingBalance.style.background = "#E3E3E3";
+  }
+};
 
-  $("#checkingWithdraw").click(function(event){
-    // Any code you put in here will be run when the checkingWithdraw button is clicked
-    if ((checkingBalance.textContent.slice(1) - parseInt(checkingAmount.val())) < 0) {
-    } else {
-      checkingBalance.textContent = "$" + (parseInt(checkingBalance.textContent.slice(1),10) - parseInt(checkingAmount.val()));
-    }
-    if (checkingBalance.textContent === "$0") {
-      checkingBalance.style.background = "red";
-    }
-  });
+function depositSavings(){
+  var savingsBalance = $("#savingsBalance")[0];
+  var savingsAmount = $("#savingsAmount");
+  savingsBalance.textContent = "$" + (parseInt(savingsBalance.textContent.slice(1),10) + parseInt(savingsAmount.val()));
+  if (savingsBalance.textContent !== "$0") {
+    savingsBalance.style.background = "#E3E3E3";
+  }
+};
 
-  $("#savingsWithdraw").click(function(event){
-    // Any code you put in here will be run when the savingsWithdraw button is clicked
-    if ((savingsBalance.textContent.slice(1) - parseInt(savingsAmount.val())) <0){
-    } else {
-      savingsBalance.textContent = "$" + (parseInt(savingsBalance.textContent.slice(1),10) - parseInt(savingsAmount.val()));
-    }
-    if (savingsBalance.textContent === "$0") {
-      savingsBalance.style.background = "red";
-    }
-  });
+function withdrawChecking(){
+  var checkingBalance = $("#checkingBalance")[0];
+  var checkingAmount = $("#checkingAmount");
+  if ((checkingBalance.textContent.slice(1) - parseInt(checkingAmount.val())) < 0) {
+  } else {
+    checkingBalance.textContent = "$" + (parseInt(checkingBalance.textContent.slice(1),10) - parseInt(checkingAmount.val()));
+  }
+  if (checkingBalance.textContent === "$0") {
+    checkingBalance.style.background = "red";
+  }
+};
 
+function withdrawSavings(){
+  var savingsBalance = $("#savingsBalance")[0];
+  var savingsAmount = $("#savingsAmount");
+  if ((savingsBalance.textContent.slice(1) - parseInt(savingsAmount.val())) <0){
+  } else {
+    savingsBalance.textContent = "$" + (parseInt(savingsBalance.textContent.slice(1),10) - parseInt(savingsAmount.val()));
+  }
+  if (savingsBalance.textContent === "$0") {
+    savingsBalance.style.background = "red";
+  }
 };
 
